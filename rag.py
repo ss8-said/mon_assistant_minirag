108a3660431a4c59e2d928b6859be888dac0e66c
said meziani <meziani12345@gmail.com>
2025-08-08 18:15:28 +0100

    Update rag.py

--- rag.py
+++ rag.py
@@ -1,6 +1,6 @@
 from langchain_community.document_loaders import TextLoader
 from langchain_community.vectorstores import Chroma
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.llms import HuggingFaceHub
 
 
